31bd8789c224a6cba04a369f861b8382387c4a6e
grzesiekszymanski <grzesieks111@tlen.pl>
2023-08-06 19:39:30 +0200

    Added average coin prioce management and update functionality in crypto serializer

--- crypto_portfolio_service_REST_API/crypto_portfolio/serializers.py
+++ crypto_portfolio_service_REST_API/crypto_portfolio/serializers.py
@@ -42,16 +42,49 @@ class CryptocurrencySerializer(serializers.ModelSerializer):
         """Calculate current worth of added cryptocurrency in USD."""
         return coin_price_usd * amount
 
+    @staticmethod
+    def _get_coin_names_from_portfolio(user):
+        """Return coin list of already existing coins in selected user portfolio."""
+        return [coin.name for coin in user.crypto.all()]
+
+    @staticmethod
+    def _calculate_average_price(average_price_before, current_price, coin_amount_before, coin_amount_to_add):
+        """Return average price for coin if user added it before."""
+        return round((coin_amount_before * average_price_before + coin_amount_to_add * current_price), 2) / \
+            (coin_amount_before + coin_amount_to_add)
+
+    def _get_coin_for_update(self, user, coin_name):
+        """Get and return selected coin for update from user portfolio."""
+        available_coins = self._get_coin_names_from_portfolio(user)
+        index = available_coins.index(coin_name)
+        return user.crypto.all()[index]
+
     def create(self, validated_data):
         """Create cryptocurrency in authenticated user portfolio."""
         # Get and calculate cryptocurrency parameters.
         user = self.context['request'].user
         coin_name = validated_data['name']
+        coin_amount = validated_data['amount']
+        portfolio_coins = self._get_coin_names_from_portfolio(user)
+
+        # Calculate coin properties.
         coin_price_usd = self._get_coin_price(coin_name)
         worth = self._calculate_worth_of_added_coin(coin_price_usd, validated_data['amount'])
 
         # Set cryptocurrency parameters.
-        validated_data['price'] = coin_price_usd
-        validated_data['worth'] = worth
+        if coin_name in portfolio_coins:
+            coin_for_update = self._get_coin_for_update(user, coin_name)
+            coin_for_update.price = self._calculate_average_price(coin_for_update.price,
+                                                                  coin_price_usd,
+                                                                  coin_for_update.amount,
+                                                                  coin_amount)
+            coin_for_update.amount += coin_amount
+            coin_for_update.worth += worth
 
-        user.crypto.create(**validated_data)
+            coin_for_update.save()
+        else:
+            validated_data['price'] = coin_price_usd
+            validated_data['worth'] = worth
+            user.crypto.create(**validated_data)
+
+        return None
